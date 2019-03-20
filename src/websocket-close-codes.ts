d2d5e27ee932bfb3a6ea461b0e17631e81480499
Eric Crosson <eric.s.crosson@utexas.edu>
2019-03-20 09:51:42 -0500

    Formatting

--- src/websocket-close-codes.ts
+++ src/websocket-close-codes.ts
@@ -11,60 +11,90 @@
  * https://www.iana.org/assignments/websocket/websocket.xml#close-code-number
  */
 enum WSCloseCode {
-    /** Normal closure; the connection successfully completed whatever
-     * purpose for which it was created. */
+    /**
+     * Normal closure; the connection successfully completed whatever
+     * purpose for which it was created.
+     */
     NormalClosure = 1000,
-    /** The endpoint is going away, either because of a server failure
+    /**
+     * The endpoint is going away, either because of a server failure
      * or because the browser is navigating away from the page that
-     * opened the connection. */
+     * opened the connection.
+     */
     GoingAway = 1001,
-    /** The endpoint is terminating the connection due to a protocol
-     * error. */
+    /**
+     * The endpoint is terminating the connection due to a protocol
+     * error.
+     */
     ProtocolError = 1002,
-    /** The connection is being terminated because the endpoint
+    /**
+     * The connection is being terminated because the endpoint
      * received data of a type it cannot accept (for example, a
-     * text-only endpoint received binary data). */
+     * text-only endpoint received binary data).
+     */
     UnsupportedData = 1003,
-    /** (Reserved.)  Indicates that no status code was provided even
-     * though one was expected. */
+    /**
+     * (Reserved.)  Indicates that no status code was provided even
+     * though one was expected.
+     */
     NoStatusRecvd = 1005,
-    /** (Reserved.) Used to indicate that a connection was closed
+    /**
+     * (Reserved.) Used to indicate that a connection was closed
      * abnormally (that is, with no close frame being sent) when a
-     * status code is expected. */
+     * status code is expected.
+     */
     AbnormalClosure = 1006,
-    /** The endpoint is terminating the connection because a message
+    /**
+     * The endpoint is terminating the connection because a message
      * was received that contained inconsistent data (e.g., non-UTF-8
-     * data within a text message). */
-    Invalidframepayloaddata = 1007,
-    /** The endpoint is terminating the connection because it received
+     * data within a text message).
+     */
+    InvalidFramePayloadData = 1007,
+    /**
+     * The endpoint is terminating the connection because it received
      * a message that violates its policy. This is a generic status
-     * code, used when codes 1003 and 1009 are not suitable. */
+     * code, used when codes 1003 and 1009 are not suitable.
+     */
     PolicyViolation = 1008,
-    /** The endpoint is terminating the connection because a data
-     * frame was received that is too large. */
-    Messagetoobig = 1009,
-    /** The client is terminating the connection because it expected
+    /**
+     * The endpoint is terminating the connection because a data frame
+     * was received that is too large.
+     */
+    MessageTooBig = 1009,
+    /**
+     * The client is terminating the connection because it expected
      * the server to negotiate one or more extension, but the server
-     * didn't. */
+     * didn't.
+     */
     MissingExtension = 1010,
-    /** The server is terminating the connection because it
-     * encountered an unexpected condition that prevented it from
-     * fulfilling the request. */
+    /**
+     * The server is terminating the connection because it encountered
+     * an unexpected condition that prevented it from fulfilling the
+     * request.
+     */
     InternalError = 1011,
-    /** The server is terminating the connection because it is
-     * restarting. [Ref] */
+    /**
+     * The server is terminating the connection because it is
+     * restarting. [Ref]
+     */
     ServiceRestart = 1012,
-    /** The server is terminating the connection due to a temporary
+    /**
+     * The server is terminating the connection due to a temporary
      * condition, e.g. it is overloaded and is casting off some of its
-     * clients. */
+     * clients.
+     */
     TryAgainLater = 1013,
-    /** The server was acting as a gateway or proxy and received an
+    /**
+     * The server was acting as a gateway or proxy and received an
      * invalid response from the upstream server. This is similar to
-     * 502 HTTP Status Code. */
+     * 502 HTTP Status Code.
+     */
     BadGateway = 1014,
-    /** (Reserved.) Indicates that the connection was closed due to a
+    /**
+     * (Reserved.) Indicates that the connection was closed due to a
      * failure to perform a TLS handshake (e.g., the server
-     * certificate can't be verified). */
+     * certificate can't be verified).
+     */
     TLSHandshake = 1015
 }
 
